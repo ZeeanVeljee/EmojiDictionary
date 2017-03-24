c2a3c2761123a483a0e36c55d39a57be17ec0d39
Zeean Veljee <Zeean@Zeeans-MacBook-Pro.local>
2017-03-25 00:13:38 +0530

    basic emoji array and DetailViewController added

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var emoTableView: UITableView!
+    var emojiArray = ["🚶", "👘", "👗", "🐴", "🔥", "🍕"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -28,7 +30,25 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return emojiArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojiArray[indexPath.row]
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let emoji = emojiArray[indexPath.row]
+        performSegue(withIdentifier: "detailSegue", sender: emoji)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //print(sender!)
+        let detailVC = segue.destination as! DetailViewController
+        detailVC.emojiData = sender as! String
     }
 
 
